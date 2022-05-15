3b1a019b6cc9f6ee7583d144bfed931c49c69922
frank martinez <frank@lastmyle.co>
2022-05-14 19:20:15 -0700

    Updates

---
+++ precache-manifest.f3cc30ce7d9055791801cefc876bcd2d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5be96b3c2d8613259dd3",
+    "url": "/INFOCapstone-Skillet-Database-System/static/css/main.e45bd68c.chunk.css"
+  },
+  {
+    "revision": "5be96b3c2d8613259dd3",
+    "url": "/INFOCapstone-Skillet-Database-System/static/js/main.74041a16.chunk.js"
+  },
+  {
+    "revision": "12237155f13b3ae3394b",
+    "url": "/INFOCapstone-Skillet-Database-System/static/js/runtime~main.7cc0a696.js"
+  },
+  {
+    "revision": "a8b2e504511493bd887c",
+    "url": "/INFOCapstone-Skillet-Database-System/static/js/2.50d47c39.chunk.js"
+  },
+  {
+    "revision": "655a1df2214623ff1b32",
+    "url": "/INFOCapstone-Skillet-Database-System/static/js/3.120d7e7f.chunk.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/INFOCapstone-Skillet-Database-System/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "351e538f44110d6d4d5e349b260ef598",
+    "url": "/INFOCapstone-Skillet-Database-System/index.html"
+  }
+];
